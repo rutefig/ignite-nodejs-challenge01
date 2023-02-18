eeea4600edee41239fcd0b314f46725a8e5905d5
Rute Figueiredo <rute.figueiredo92@gmail.com>
2023-02-18 19:39:26 +0000

    first draft of routes for GET and POST of /tasks

--- src/server.js
+++ src/server.js
@@ -1,7 +1,41 @@
+import { randomUUID } from 'node:crypto'
 import http from 'node:http'
+import { Database } from './database.js'
+import { json } from './middlewares/json.js'
+
+const database = new Database()
 
 const server = http.createServer(async (req, res) => {
-    return res.end()
+    const { method, url } = req
+
+    await json(req, res)
+
+    if (method === 'GET' && url === '/tasks') {
+        
+        const tasks = database.select("tasks")
+        
+        return res.end(JSON.stringify(tasks))
+    }
+
+    if (method === 'POST' && url === '/tasks') {
+        
+        const { title, description } = req.body
+
+        const task = {
+            id: randomUUID(),
+            title,
+            description,
+            completedAt: null,
+            createdAt: new Date(),
+            updatedAt: null
+        }
+
+        database.insert("tasks", task)
+
+        return res.writeHead(201).end()
+    }
+
+    return res.writeHead(404).end()
 })
 
 server.listen(3333, () => {
